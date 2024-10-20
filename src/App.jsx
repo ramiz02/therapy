4547f08955ea7fb8f46909010423f86406f2d2bc
ramiz02 <skramizali01@gmail.com>
2024-10-20 19:27:12 +0530

    code

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import './App.css'
+
+const App = () =>{
+  return(<>
+    <div className='bg-container'>
+        <div>
+          <img src='https://res.cloudinary.com/dqvvpyz72/image/upload/v1729346849/Picsart_24-10-18_18-59-07-845_1_ai7ltx.jpg' alt="yoga" className='image'/>
+        </div>
+        <section>
+          <h2 className='description'>The integrated approach to yoga therapy (IAYT) model is based on the principle that “<span className='heading'>the root of all psychosocial illnesses is in the mind; which causes an internal imbalance due to long standing stressful and demanding situations of life.</span>”</h2>
+          <h2>This model uses Pancha Kosha as a foundation to understand human existence at different levels and development of illness through the complex interaction of these levels and external life style factors.  IAYT slows down the loop of uncontrolled speed of thoughts (stress) through several techniques that use the principle of “successive stimulations followed by progressive relaxation and the rest” to correct the imbalances, promote “mastery over the mind” and harmonize the disturbances at each of the five levels (Pancha Kosa). Based on the above Pancha Kosa concept, the IAYT model incorporates varied yogic practices at each level to help patients with different disorders deal with their problems. The integrated approach to yoga therapy (IAYT) model looks at health from a holistic perspective (vs reductionist approach in addressing symptoms only) and to restore health at all levels.</h2>
+        </section>
+        <section>
+          <h2 className='description'>Recommended Yoga therapy practices are based on research for over 4 decades with safety is tailor made to meet individual needs and administered under expert Yoga therapists. Yogic counseling based on rich knowledge of texts of Yoga comprising of Jnana Yoga, Bhakti Yoga, Karma Yoga and Raja Yoga are used.</h2>
+        </section>
+        <section>
+          <h2>Our medical yoga therapy in Kolkata is lead under the guidance of world renowned yoga therapist /yoga scientist/yoga authority. 
+          <span className='doctor'>DR.RAMANATH ROY CHOWDHURY(PHD)</span> with over 25 years of experience.</h2>
+        </section>
+        <section>
+          <ol className='list-type'>
+            <li className='list-text'><span className='text-bold'>Oncology:</span> Breast, Lung cancer, brain, Colon, Prostate and other cancers.</li>
+            <li className='list-text'><span className='text-bold'>Mental Health:</span> Anxiety and Depression</li>
+            <li className='list-text'><span className='text-bold'>Rheumatology:</span> Arthritis, Knee Pain, post knee surgery rehab</li>
+            <li className='list-text'><span className='text-bold'>Spinal disorders:</span> Acute and Chronic Back Pain, Lumbar Spondylosis, Disc Bulge, Disc Prolapse, Scoliosis, Neck Pain</li>
+            <li className='list-text'><span className='text-bold'>Gastroenterology:</span> Gastritis, Peptic Ulcer, Irritable Bowel Syndrome (IBS)</li>
+            <li className='list-text'><span className='text-bold'>Metabolic disorders: </span> Obesity, Diabetes mellitus and thyroid problems</li>
+            <li className='list-text'><span className='text-bold'>Neurology: </span> Epilepsy, Migraine headaches</li>
+            <li className='list-text'><span className='text-bold'>General Health : </span> Promotion of Positive Health</li>
+            <li className='list-text'><span className='text-bold'>Yoga for pregnancy</span></li>
+            <li className='list-text'><span className='text-bold'>Yoga for children</span></li>
+          </ol>
+        </section>
+        <div className='note'>
+          <h2 className='text'>No yoga no peace<br/>
+          Know yoga know peace</h2>
+        </div>
+    </div>
+    <footer className='footer'>
+  <div>
+    <h2>Designed & Developed by <a href=''>SK RAMIZ ALI</a></h2>
+    <p className='Paragraph'>I am a skilled Web Designer and Full-Stack Developer dedicated to creating dynamic, user-friendly websites that help your business succeed. Let’s build something great together!</p>
+    <p className='Paragraph'><strong>Contact:</strong> <a href=''>7003136278</a></p>
+    <h3><strong>*Know About Me</strong>- Click Below Image</h3>
+    <div>
+    <a href='https://www.linkedin.com/feed/' target='_blank'><img src='https://assets.ccbp.in/frontend/react-js/nxt-watch-linked-in-logo-img.png' alt="linkedin" className='my-image'/></a>
+    <a href='https://youtu.be/R7QsV3vQbxg?si=0P1gPcgod1ppsm8K' target='_blank'><img src='https://res.cloudinary.com/dqvvpyz72/image/upload/v1729429665/youtube-logo-png-15_mywlgp.png' alt="youtube" className='my-image'/></a>
+    </div>
+  </div>
+  <div>
+    <img src='https://res.cloudinary.com/dqvvpyz72/image/upload/v1729427575/Ramiz_Photo_efm0ji.jpg' alt='SK RAMIZ ALI' className='my-image' />
+  </div>
+</footer>
+
+    </>
+  )
+}
+
+export default App
